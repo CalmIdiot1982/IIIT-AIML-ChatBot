bfb30fead3aaf3875f35e043a6d9610ed7da1b44
soumya panda <soumyapanda1982@gmail.com>
2020-12-26 11:43:03 +0530

    mail utility

---
+++ simple_mail.py
@@ -0,0 +1,18 @@
+import smtplib
+
+sender_email = "skpandaupgrad@gmail.com"
+reciever_email = "soumyapanda1982@gmail.com"
+password = "Cobol1982$"
+message = "This message was sent from simple_email"
+
+
+def mail_results(emailid=reciever_email,html_msg="blank message"):
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.starttls()
+	try:
+		server.login(sender_email, password)
+		print("got to send email: ", emailid)
+		server.sendmail(sender_email,emailid,html_msg)
+	except Exception as e:
+		print("Exception was hit :", e)
+
